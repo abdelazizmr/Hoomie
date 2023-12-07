1462740b774df468e56f5c05d011fa97c2346cf1
hamza riahi <hamza.ouladelhadjriahi@virtuocode.com>
2023-12-07 12:41:49 +0100

    i get the data from the data base

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InterestController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -45,3 +46,6 @@ Route::put('/update-privacy', [ProfileController::class,'updatePrivacy'])->name(
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/listings',[ListingsController::class,'index'])->name('app.index');
